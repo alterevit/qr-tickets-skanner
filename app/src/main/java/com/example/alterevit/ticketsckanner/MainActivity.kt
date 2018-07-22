753a0792d3ee1520e9950f7eb5f4ca747576dc08
Alterevit <krb.strit@gmail.com>
2018-07-22 23:45:03 +0300

    update Ui

--- app/src/main/java/com/example/alterevit/ticketsckanner/MainActivity.kt
+++ app/src/main/java/com/example/alterevit/ticketsckanner/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.alterevit.ticketsckanner
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.Result
-import kotlinx.android.synthetic.main.main_ui.*
 
 class MainActivity : Activity() {
 
@@ -18,23 +14,20 @@ class MainActivity : Activity() {
 
     override fun onResume() {
         super.onResume()
-        with(qr_scanner) {
-            setAutoFocus(true)
-            flash = false
-            setShouldScaleToFill(true)
-            setFormats(listOf(BarcodeFormat.QR_CODE))
-            setResultHandler { result: Result ->
-                Log.i(TAG,  "text: ${result.text} \nbarcodeFormat: ${result.barcodeFormat}")
-                resumeCameraPreview {
-
-                }
-            }
-            startCamera()
-        }
+//        with(qr_scanner) {
+//            setAutoFocus(true)
+//            flash = false
+//            setShouldScaleToFill(true)
+//            setFormats(listOf(BarcodeFormat.QR_CODE))
+//            setResultHandler { result: Result ->
+//                Log.i(TAG,  "text: ${result.text} \nbarcodeFormat: ${result.barcodeFormat}")
+//            }
+//            startCamera()
+//        }
     }
 
     override fun onPause() {
         super.onPause()
-        qr_scanner.stopCamera()
+        // qr_scanner.stopCamera()
     }
 }

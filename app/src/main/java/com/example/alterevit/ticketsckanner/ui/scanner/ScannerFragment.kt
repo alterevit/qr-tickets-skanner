9b69721e9712ef148ee278e5ac9cc4bc4faffec8
Alterevit <krb.strit@gmail.com>
2018-07-23 01:05:24 +0300

    implemented forward to TicketFragment after Scaning

--- app/src/main/java/com/example/alterevit/ticketsckanner/ui/scanner/ScannerFragment.kt
+++ app/src/main/java/com/example/alterevit/ticketsckanner/ui/scanner/ScannerFragment.kt
@@ -1,14 +1,14 @@
 package com.example.alterevit.ticketsckanner.ui.scanner
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.alterevit.ticketsckanner.R
 import com.example.alterevit.ticketsckanner.extension.inflate
+import com.example.alterevit.ticketsckanner.extension.replaceFragment
+import com.example.alterevit.ticketsckanner.ui.ticket.TicketFragmet
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.Result
 import kotlinx.android.synthetic.main.fragment_scanner.*
@@ -33,7 +33,7 @@ class ScannerFragment : Fragment()  {
             setShouldScaleToFill(true)
             setFormats(listOf(BarcodeFormat.QR_CODE))
             setResultHandler { result: Result ->
-                Log.i(TAG,  "text: ${result.text} \nbarcodeFormat: ${result.barcodeFormat}")
+                activity?.replaceFragment(TicketFragmet.getInstance(result.text))
             }
             startCamera()
         }
